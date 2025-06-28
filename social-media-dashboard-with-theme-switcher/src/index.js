215e4abc536b22c5eb8ffc0e4ae1b82e0e136ef7
thelastmedici <christianopeyemijoshua@gmail.com>
2025-06-28 03:05:53 +0100

    add toggle

--- social-media-dashboard-with-theme-switcher/src/index.js
+++ social-media-dashboard-with-theme-switcher/src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    console.log("lets code!!!");
+    const toggle = document.getElementById('toggleTheme');
+    const track = document.getElementById('track');
+    const thumb = document.getElementById('thumb');
+
+
+
+    toggle.addEventListener('click', () => {
+        if(toggle.checked){
+            thumb.classList.add("translate-x-full", "bg-white");
+            track.classList.remove("bg-gradient-to-r", "from-blue-500", "to-green-400");
+            track.classList.add("bg-gray-300")
+        }else{
+             thumb.classList.remove("translate-x-full", "bg-white");
+             thumb.classList.add("left-1", "bg-very-dark-blue-bg");
+            track.classList.remove("bg-gray-300");
+            track.classList.add("bg-gradient-to-r", "from-blue-500", "to-green-400");
+        }
+    })
 })
