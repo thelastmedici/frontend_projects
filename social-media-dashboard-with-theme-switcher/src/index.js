b1543544c790aee7e38f984731ea8e56c11d3d02
thelastmedici <christianopeyemijoshua@gmail.com>
2025-07-05 17:17:58 +0100

    added desktop design

--- social-media-dashboard-with-theme-switcher/src/index.js
+++ social-media-dashboard-with-theme-switcher/src/index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const toggle = document.getElementById('toggleTheme');
     const track = document.getElementById('track');
     const thumb = document.getElementById('thumb');
+    const blackText = document.querySelector('.black-text');
 
 
 
@@ -9,12 +10,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
         if(toggle.checked){
             thumb.classList.add("translate-x-full", "bg-white");
             track.classList.remove("bg-gradient-to-r", "from-blue-500", "to-green-400");
-            track.classList.add("bg-gray-300")
+            track.classList.add("bg-gray-300");
+            h1.classList.add("text-black");
         }else{
              thumb.classList.remove("translate-x-full", "bg-white");
              thumb.classList.add("left-1", "bg-very-dark-blue-bg");
             track.classList.remove("bg-gray-300");
             track.classList.add("bg-gradient-to-r", "from-blue-500", "to-green-400");
+             h1.classList.remove("text-black");
         }
     })
 })
