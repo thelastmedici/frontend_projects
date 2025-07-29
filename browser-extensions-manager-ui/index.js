c5d68fdb3741b19f7dce87f8ad5ac3bd62a47a29
thelastmedici <christianopeyemijoshua@gmail.com>
2025-07-29 20:18:57 +0100

    add light theme for test

--- browser-extensions-manager-ui/index.js
+++ browser-extensions-manager-ui/index.js
@@ -2,14 +2,19 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const themeToggle = document.querySelector('.themeswitcher');
     
+    
     themeToggle.addEventListener('click', ()=>{
         const darkMode = document.body.classList.contains('bg-dark-gradient');
+        const extList = document.querySelector('.extension-list');
         if (darkMode){
             document.body.classList.remove('bg-dark-gradient');
             document.body.classList.add('bg-white');
+            extList.classList.add('text-black')
+            
         } else{
              document.body.classList.remove('bg-white');
             document.body.classList.add('bg-dark-gradient');
+            extList.classList.remove('text-black')
         }
     })
 })
