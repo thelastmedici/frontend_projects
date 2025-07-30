9879cb3d1d10bcc840b5a094f6017630e5b58e4f
thelastmedici <christianopeyemijoshua@gmail.com>
2025-07-30 02:10:15 +0100

    add light theme

--- browser-extensions-manager-ui/index.js
+++ browser-extensions-manager-ui/index.js
@@ -1,23 +1,31 @@
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    const themeToggle = document.querySelector('.themeswitcher');
+document.addEventListener('DOMContentLoaded', ()=>{     
+    const themeToggle = document.querySelector('.themeswitcher');          
     
-    
-    themeToggle.addEventListener('click', ()=>{
-        const darkMode = document.body.classList.contains('bg-dark-gradient');
-        const extList = document.querySelector('.extension-list');
-        const logoImg = document.getElementById('logo-img');
-        if (darkMode){
-            document.body.classList.remove('bg-dark-gradient');
-            document.body.classList.add('bg-white');
-            extList.classList.add('text-black');
-            logoImg.src =logoImg.src = '../assets/images/extensions_logo_black_text.svg';
-            
-        } else{
-             document.body.classList.remove('bg-white');
-            document.body.classList.add('bg-dark-gradient');
-            extList.classList.remove('text-black');
-            logoImg.src = "../assets/images/logo.svg";
-        }
-    });
-})
+    themeToggle.addEventListener('click', ()=>{         
+        const darkMode = document.body.classList.contains('bg-dark-gradient');         
+        const extList = document.querySelector('.extension-list');         
+        const logoImg = document.getElementById('logo-img');         
+        const themeLogo = document.querySelector('.themeswitcher img');
+        const switchTheme = document.getElementById('switcher');
+        
+        
+                  
+        if (darkMode){             
+            document.body.classList.remove('bg-dark-gradient');             
+            document.body.classList.add('bg-light-gradient');             
+            extList.classList.add('text-black');             
+            logoImg.src = '../assets/images/extensions_logo_black_text.svg';             
+            themeLogo.src = '../assets/images/icon-moon.svg';   
+            switchTheme.classList.remove('bg-neutral-700');
+            switchTheme.classList.add('bg-white');                   
+        } else{             
+            document.body.classList.remove('bg-light-gradient');             
+            document.body.classList.add('bg-dark-gradient');             
+            extList.classList.remove('text-black');             
+            logoImg.src = "../assets/images/logo.svg";             
+            themeLogo.src = '../assets/images/icon-sun.svg';
+            switchTheme.classList.add('bg-neutral-700');
+            switchTheme.classList.remove('bg-white');        
+        }     
+    }); 
+});
