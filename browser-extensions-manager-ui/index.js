a4cb55232e4294a46205d9604ada5be1e7816fef
thelastmedici <christianopeyemijoshua@gmail.com>
2025-08-01 09:45:19 +0100

    rewrite theme extension

--- browser-extensions-manager-ui/index.js
+++ browser-extensions-manager-ui/index.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const isDarkMode = body.classList.contains('bg-dark-gradient');
             const themeIcon = document.getElementById('theme-icon');
             const switcher = document.getElementById('switcher');
+            const darkLogo = document.querySelector('.dark-logo')
             
             if (isDarkMode){
                 body.classList.remove('bg-dark-gradient', 'text-neutral-100');
@@ -14,15 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
                 themeIcon.src = '../assets/images/icon-moon.svg';
+                darkLogo.src = '../assets/images/extensions_logo_black_text.svg';
 
                 switcher.classList.remove('bg-neutral-700');
                 switcher.classList.add('bg-white', 'shadow-lg');
+
 
             } else {
                 body.classList.add('bg-dark-gradient', 'text-neutral-100');
                 body.classList.remove('bg-light-gradient', 'text-gray-900');
 
                 themeIcon.src = '../assets/images/icon-sun.svg';
+                darkLogo.src = '../assets/images/logo.svg';
 
 
                 switcher.classList.add('bg-neutral-700');
